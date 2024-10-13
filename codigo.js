d3e3936e05f58d458ed94f232f5c269f8a085613
CristhianAmaya <est.cristhian.amaya@unimilitar.edu.co>
2024-10-12 19:15:24 -0500

    commit de prueba

--- codigo.js
+++ codigo.js
@@ -1,2 +1,16 @@
-let numero = 29;
-console.log(numero);
+// Crea un algoritmo que imprima numeros impares
+let numeros = new Array(10);
+
+for (let i = 0; i < numeros.length; i++) {
+    numeros[i] = Math.floor(Math.random() * (29 - 11 + 1)) + 11; // Numeros generados entre 11 a 29
+}
+
+function imparesLet(){
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 != 0) {
+            console.log('Numero impar en la posición', i, ' = ', numeros[i]);
+        }
+    }
+}
+
+imparesLet();
